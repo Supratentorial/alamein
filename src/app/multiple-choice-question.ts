7d58e035c45b034fca5af6ac57837dc19e918fa3
rmit-s3490462-blake-cramer <s3490462@student.rmit.edu.au>
2016-10-15 16:23:01 +1100

    cramers commit

---
+++ src/app/multiple-choice-question.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './multiple-choice-question.html',
+})
+export class MultipleChoiceQuestion {
+  question = 'What colour is your car?';
+}
