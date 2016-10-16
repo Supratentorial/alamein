e9d2e6cbed91d7fc0d8ea4b0dcf902299d60b764
rmit-s3490462-blake-cramer <s3490462@student.rmit.edu.au>
2016-10-16 22:38:03 +1100

    Cramer commit

--- src/app/multiple-choice-question.component.ts
+++ src/app/multiple-choice-question.component.ts
@@ -1,17 +1,24 @@
 import { Component } from '@angular/core';
 import { Mcq } from './mcq'; 
 import { OnInit } from '@angular/core';
+import { McqDataService} from './mcq-data.service';
 
 @Component({
   selector: 'multiple-choice-question',
   templateUrl: './multiple-choice-question.component.html',
 })
 export class MultipleChoiceQuestionComponent implements OnInit {
-  ngOnInit() {}
-  currentMcq : Mcq = {
-    id: 1,
-    question: "Are you hungry?"
+  ngOnInit() {this.mcqdataservice.getMcqs.subscribe(this.getMcqsSuccess, this.getMcqsError)}
+  
+  constructor (private mcqdataservice: McqDataService) {}
+  
+  getMcqsError(){
+    console.log("something bad happened")
   }
+  getMcqsSuccess(){
+    console.log("something good happened")
+  }
+  currentMcq : Mcq;
 
   question = 'What colour is your car?';
   question2 = 'What is your favourite food?';
