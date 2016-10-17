bc5f551e416a6973ed27f33699dac73d3a05515a
Blake Mumford <blake.mumford@indoctrinate.com.au>
2016-10-17 21:40:06 +1100

    Horizontal center alignment of content

--- src/app/create-mcq.component.ts
+++ src/app/create-mcq.component.ts
@@ -5,7 +5,8 @@ import { McqDataService} from './mcq-data.service';
 
 @Component({
     selector: 'create-mcq',
-    templateUrl: './create-mcq.component.html'
+    templateUrl: './create-mcq.component.html',
+    styleUrls: ['./create-mcq.component.scss']
 })
 export class CreateMcqComponent  implements OnInit{
     ngOnInit(){
